e88116da04edc5aab1b423e8cbaf7da436dca4bc
swathigoru <swathi.goru@gmail.com>
2017-05-23 17:47:52 -0700

    Initial commit

---
+++ app/src/main/java/com/classprojects/navigationdrawer/FirstFragment.java
@@ -0,0 +1,25 @@
+package com.classprojects.navigationdrawer;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Swathy on 5/22/17.
+ */
+
+public class FirstFragment extends Fragment {
+
+    View myView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.first_layout,container, false);
+        return myView;
+    }
+
+}
+
